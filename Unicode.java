8b0d52a22e2102bb6e8c5608d0e6b7ecba24b010
Miguel Ángel López Moyano <i02lomom@gmail.com>
2015-12-05 15:26:34 +0100

    Create Unicode.java

---
+++ Unicode.java
@@ -0,0 +1,13 @@
+/**
+ * Muestra la tabla de caracteres Unicode. Utiliza el carácter tabulador.
+ *
+ * @author Miguel Angel López Moyano
+ * @version 1.0
+ */
+public class Unicode{
+	public static void main(String[] args){
+		for(int i=0;i<=255;i++){
+			System.out.print((char)i+"\t");
+		}
+	}
+}
